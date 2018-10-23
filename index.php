a1e7a0a762c19114608d116ea7c62c4bbc0318c4
Keny Oshiro <keny.oshiro92@gmail.com>
2018-10-23 00:45:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php include_once("index.html"); ?>
+<?php
+include_once("index.html"); 
+$url = parse_url(getenv("mysql://b1c8fef3da3794:5b9b40b6@us-cdbr-iron-east-01.cleardb.net/heroku_75916913ce210ba?reconnect=true"));
+
+$server = $url["us-cdbr-iron-east-01.cleardb.net"];
+$username = $url["b1c8fef3da3794:5b9b40b6"];
+$password = $url["5b9b40b6"];
+$db = substr($url["heroku_75916913ce210ba"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+?>
